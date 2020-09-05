6005f376bfe3c01c13e0e3b5131154f4c2f89be8
panka <pankajshinde798@gmail.com>
2020-09-05 18:14:37 +0530

    2nd

--- djangorestapi/settings.py
+++ djangorestapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,8 +55,7 @@ ROOT_URLCONF = 'djangorestapi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,8 +114,9 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'core.User'
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangorestapi/static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
